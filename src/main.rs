a8d40ff7a0d3e2c7ca0f6695d756f530f67f08a0
Warren Hood <nullbyte001@gmail.com>
2022-08-20 20:04:54 +0200

    Optimised successive jumps and arithmetic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum Op {
-    Inc,
-    Dec,
-    Next,
-    Prev,
+    Inc(u8),
+    Dec(u8),
+    Next(usize),
+    Prev(usize),
     LoopStart,
     LoopEnd,
     Print,
@@ -18,10 +18,10 @@ fn lex(prog: &str) -> Vec<Op>{
     let mut ops: Vec<Op> = vec![];
     for ch in prog.chars() {
         match ch {
-            '>' => {ops.push(Next)},
-            '<' => {ops.push(Prev)},
-            '+' => {ops.push(Inc)},
-            '-' => {ops.push(Dec)},
+            '>' => {ops.push(Next(1))},
+            '<' => {ops.push(Prev(1))},
+            '+' => {ops.push(Inc(1))},
+            '-' => {ops.push(Dec(1))},
             '[' => {ops.push(LoopStart)},
             ']' => {ops.push(LoopEnd)},
             ',' => {ops.push(Read)},
@@ -30,6 +30,119 @@ fn lex(prog: &str) -> Vec<Op>{
         }
     }
     ops
+}
+
+fn optimize(ops: Vec<Op>) -> Vec<Op> {
+    let mut new_ops: Vec<Op> = vec![];
+    let mut i: usize = 0;
+    let mut last_op: Option<&Op> = None;
+    let mut add_diff: i16 = 0;
+    let mut jmp_diff: isize = 0;
+    while i < ops.len() {
+        let this_op: &Op = &ops[i];
+        let mut this_arithmetic: bool = false;
+        let mut this_jump: bool = false;
+        let mut last_arithmetic: bool = false;
+        let mut last_jump: bool = false;
+        if *this_op == Inc(1) || *this_op == Dec(1) {
+            this_arithmetic = true;
+        }
+        else if *this_op == Next(1) || *this_op == Prev(1) {
+            this_jump = true;
+        }
+        match last_op {
+            Some(last) => {
+                if *last == Inc(1) || *last == Dec(1) {
+                    last_arithmetic = true;
+                }
+                else if *last == Next(1) || *last == Prev(1) {
+                    last_jump = true;
+                }
+            },
+            _ => {}
+        }
+
+        // Continue with arithmetic stuff
+        if this_arithmetic {
+            if *this_op == Inc(1) {
+                add_diff += 1;
+            }
+            else if *this_op == Dec(1) {
+                add_diff -= 1;
+            }
+
+            if last_jump {
+                if jmp_diff > 0 {
+                    new_ops.push(Next(jmp_diff as usize));
+                }
+                else if jmp_diff < 0 {
+                    new_ops.push(Prev(-jmp_diff as usize));
+                }
+                jmp_diff = 0;
+            }
+        }
+        else if this_jump { // Continue with jump stuff
+            if *this_op == Next(1) {
+                jmp_diff += 1;
+            }
+            else if *this_op == Prev(1) {
+                jmp_diff -= 1;
+            }
+
+            if last_arithmetic {
+                if add_diff > 0 {
+                    new_ops.push(Inc(add_diff as u8));
+                }
+                else if add_diff < 0 {
+                    new_ops.push(Dec(-add_diff as u8));
+                }
+                add_diff = 0;
+            }
+        }
+        else {
+            // Well, this is something that isn't a jump or arithmetic op
+            
+            if last_arithmetic {
+                if add_diff > 0 {
+                    new_ops.push(Inc(add_diff as u8));
+                }
+                else if add_diff < 0 {
+                    new_ops.push(Dec(-add_diff as u8));
+                }
+                add_diff = 0;
+            }
+
+            if last_jump {
+                if jmp_diff > 0 {
+                    new_ops.push(Next(jmp_diff as usize));
+                }
+                else if jmp_diff < 0 {
+                    new_ops.push(Prev(-jmp_diff as usize));
+                }
+                jmp_diff = 0;
+            }
+
+            // Just add the next instruction as is
+            new_ops.push(*this_op);
+        }
+        last_op = Some(this_op);
+        i += 1;
+    }
+
+    if add_diff > 0 {
+        new_ops.push(Inc(add_diff as u8));
+    }
+    else if add_diff < 0 {
+        new_ops.push(Dec(-add_diff as u8));
+    }
+    if jmp_diff > 0 {
+        new_ops.push(Next(jmp_diff as usize));
+    }
+    else if jmp_diff < 0 {
+        new_ops.push(Prev(-jmp_diff as usize));
+    }
+
+    new_ops
 }
 
 fn show_mem(mem_ptr: &mut usize, mem: &mut [u8]) {
@@ -72,10 +185,10 @@ fn run(op: &Op, mem: &mut [u8], prog: &[Op], pc: &mut usize, mem_ptr: &mut usize
     // show_current_pos(pc, prog);
 
     match op {
-        Inc => { mem[*mem_ptr] += 1},
-        Dec => { mem[*mem_ptr] -= 1},
-        Next => { *mem_ptr += 1},
-        Prev => { *mem_ptr -= 1},
+        Inc(amt) => { mem[*mem_ptr] += amt},
+        Dec(amt) => { mem[*mem_ptr] -= amt},
+        Next(amt) => { *mem_ptr += amt},
+        Prev(amt) => { *mem_ptr -= amt},
         LoopStart => {
             *pc += 1;
             let loop_start = *pc;
@@ -140,7 +253,7 @@ fn main() {
     }
     let mut program_source = String::new();
     std::fs::File::open(&args[1]).expect("name of a brainfuck source file").read_to_string(&mut program_source).unwrap();
-    let program = lex(&program_source);
+    let program = optimize(lex(&program_source));
     let mut mem: [u8; 3000] = [0; 3000];
     let mut pc: usize = 0;
     let mut mem_ptr: usize = 1500;
